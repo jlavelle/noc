a19236de110e20370788d5cbecc65b744a7d6fbc
John C. Lavelle <jlavelle604@gmail.com>
2019-10-11 00:39:31 -0400

    More vector operations

--- src/vector.js
+++ src/vector.js
@@ -1,12 +1,30 @@
-import { Obj } from '@masaeedu/fp'
+import { Obj, IntSum } from '@masaeedu/fp'
 
 const Vec2 = x => y => ({ x, y })
 const Vec3 = x => y => z => ({x, y, z})
 
 const add = Obj.zipWith(a => b => a + b)
 
+const subtract = Obj.zipWith(a => b => a - b)
+
+const scale = n => Obj.map(x => x * n)
+
+const dot = v1 => v2 => Obj.fold(IntSum)(Obj.zipWith(a => b => a * b)(v1)(v2))
+
+const magnitude = v => Math.sqrt(Obj.foldMap(IntSum)(x => x * x)(v))
+
+const normalize = v => {
+  const m = magnitude(v)
+  return scale(1 / (m === 0 ? 1 : m))(v)
+}
+
 export {
   Vec2,
   Vec3,
-  add
+  add,
+  subtract,
+  scale,
+  dot,
+  magnitude,
+  normalize
 }
