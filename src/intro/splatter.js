590d5e6e4465ed9a84ae70e9e735290824a04c0f
John C. Lavelle <jlavelle604@gmail.com>
2019-10-11 11:12:08 -0400

    Splatter lights

--- src/intro/splatter.js
+++ src/intro/splatter.js
@@ -14,23 +14,31 @@ export const sketch = p => {
     p.noStroke()
     console.log('foo')
   }
-
+  
   let angle = 0
   let spheres = []
-
+  let max = 10
+  
   p.draw = () => {
     p.background(0)
+    p.directionalLight(255, 255, 255, 1, 1, 0)
+    p.directionalLight(255, 255, 255, 0, 1, -1)
     p.rotateY(angle)
+    if (p.frameRate() < 20) {
+      max -= 1
+    } else {
+      max += 10
+    }
     for (let i = 0; i <= 10; i++) {
-      if (spheres.length > 250) {
+      if (spheres.length > max) {
         spheres.shift()
       } else {
         const radius = randomGaussian() * 5 + 10
         
         const coords = configureGaussian({
           x: [200, 0],
-          y: [100, 0],
-          z: [100, 0]
+          y: [200, 0],
+          z: [200, 0]
         })
 
         const velocity = configureGaussian({
@@ -40,9 +48,9 @@ export const sketch = p => {
         })
 
         const color = Obj.map(x => x % 255)(configureGaussian({
-          r: [1, 0],
-          g: [100, 255 / 2],
-          b: [100, 255 / 2]
+          r: [500, 255 / 2],
+          g: [500, 255 / 2],
+          b: [500, 255 / 2]
         }))
 
         spheres.push({coords, velocity, color, radius})
@@ -53,7 +61,7 @@ export const sketch = p => {
       const { x, y, z } = s.coords
       const { r, g, b } = s.color
       p.push()
-      p.fill(r, g, b)
+      p.ambientMaterial(r, g, b)
       p.translate(x, y, z)
       p.sphere(s.radius)
       p.pop()
